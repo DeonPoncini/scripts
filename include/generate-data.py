153db7760ecf08eef071ad1f94295beeadb4d880
Deon Poncini <dex1337@gmail.com>
2015-01-18 18:33:39 -0800

    Generate data types in python
    We can use these to validate the card data as its read

--- include/generate-data.py
+++ include/generate-data.py
@@ -190,6 +190,46 @@ def write_java(packageObjs, enumObjs, constantObjs, structObjs, \
     # close class
     outfile.write('}\n')
 
+def write_python(packageObjs, enumObjs, constantObjs, structObjs, \
+        outPath, fileName):
+    fullPath = os.path.join(outPath, 'python')
+    if not os.path.exists(fullPath):
+        os.makedirs(fullPath)
+    outfile = codecs.open(os.path.join(fullPath, fileName + '.py'), 'w', 'utf-8')
+
+    outfile.write('#!/usr/bin/python\n')
+    # create an enum type
+    outfile.write('def enum(*sequential, **named):\n')
+    outfile.write('\tenums = dict(zip(sequential, range(len(sequential))),')
+    outfile.write('**named)\n')
+    outfile.write('\treverse = dict((value, key) for key, value in ')
+    outfile.write('enums.iteritems())\n')
+    outfile.write('\tenums[\'reverse_mapping\'] = reverse\n')
+    outfile.write('\treturn type(\'Enum\', (), enums)\n')
+
+    # enumerations
+    for e in enumObjs:
+        outfile.write(e.name + ' = enum(')
+        outstr = ''
+        for entry in e.entries:
+            outstr = outstr + '\'' + entry + '\','
+        outstr = outstr[:-1]
+        outfile.write(outstr + ')\n')
+
+    # constants
+    for c in constantObjs:
+        outfile.write(c.name + ' = ' + c.value + '\n')
+
+    # structs
+    for s in structObjs:
+        outfile.write('class ' + s.name + ':\n')
+        outfile.write('\tdef __init__(self')
+        for e in s.elements:
+            outfile.write(', ' + e.name)
+        outfile.write('):\n')
+        for e in s.elements:
+            outfile.write('\t\tself.' + e.name + ' = ' + e.name + '\n')
+
 if len(sys.argv) < 3:
     print "Usage is: " + sys.argv[0] + " <data.xml> <outpath> "
     sys.exit(1)
@@ -255,3 +295,4 @@ for s in structs:
 write_cpp(packageObj, enumObjs, constantObjs, structObjs, outPath, fileName)
 write_c(packageObj, enumObjs, constantObjs, structObjs, outPath, fileName)
 write_java(packageObj, enumObjs, constantObjs, structObjs, outPath, fileName)
+write_python(packageObj, enumObjs, constantObjs, structObjs, outPath, fileName)
